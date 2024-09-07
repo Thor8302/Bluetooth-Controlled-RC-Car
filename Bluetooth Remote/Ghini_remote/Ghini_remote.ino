a8aee98673007330314de25debe98004fb578b94
Thor8302 <68622476+Thor8302@users.noreply.github.com>
2024-09-08 03:12:58 +0530

    GHINI REMOTE PROTOCOL 14/5/2021

--- Bluetooth Remote/Ghini_remote/Ghini_remote.ino
+++ Bluetooth Remote/Ghini_remote/Ghini_remote.ino
@@ -1,38 +1,122 @@
 /*
-Ghini remote
-21/5/2021
+               GHINI REMOTE PROTOCOL
+               14/5/2021
 */
-int t=0,a;
-char b,c='S';
+
+int mtrspeed,steer,light,park,strangleint,parkkey,lightkey;
+char mtrvalue,strvalue,strangle,parkvalue='x',lightvalue='w';
 void setup() {
-    pinMode(A7,INPUT);
-    //pinMode(11,OUTPUT);
-    //digitalWrite(11,1);
-    //pinMode(10,OUTPUT);
-    //digitalWrite(10,0);
-    Serial.begin(9600);
+  pinMode(A6,INPUT);
+  pinMode(A7,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A4,INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-    t=analogRead(A7);
-    if((t>550)&&(650)){
-    a=0;
-    b='S';
+    mtrspeed=analogRead(A6);
+    steer=analogRead(A7);
+    light=analogRead(A4);
+    park=analogRead(A2);
+    if(mtrspeed>700){             //  check for forward 
+      mtrvalue='F';                         // and speed accordingly
+      mtrspeed=map(mtrspeed,700,1021,0,10);
     }
-    if(t>650){
-      a=map(t,650,1020,0,10);
-      b='F';
+    else if(mtrspeed<400){        //check for backward
+      mtrvalue='B';                           // and speed accordingly
+      mtrspeed=map(mtrspeed,5,400,10,0);
     }
-    if(t<550){
-      a=map(t,1,550,10,0);
-      b='B';
+    else{
+      mtrvalue='S';                        // else speed 
+      mtrspeed=0;
     }
-    if(a<10)
-    Serial.println(a);
-    
-    Serial.println(b);
-    if(a>=10)
-    Serial.println('q');
-    c=t;
-  delay(65);
+    if(mtrspeed==10)
+    Serial.println('q');                  // and sending motor speed
+    else
+    Serial.println(mtrspeed);
+
+if(steer>700){                      //check for right
+  strvalue='R';
+  strangleint=map(steer,702,1022,107,116);//and angle int value
+}
+else if(steer<300){                  // check for left
+  strvalue='L';
+  strangleint=map(steer,0,298,97,106); // and angle int value
+}
+else{
+  strvalue='S';
+  strangleint=106;
+}
+
+
+if(strangleint<98)
+strangleint=98;
+if(strangleint>116)
+strangleint=116;
+strangle= char (strangleint);
+Serial.println(strangle);             // and sending angle value in char
+
+
+
+if((mtrvalue=='F')&&(strvalue=='S')){      // from here 
+  Serial.println('F');
+}
+else if((mtrvalue=='F')&&(strvalue=='R')){
+  Serial.println('I');
+}
+else if((mtrvalue=='F')&&(strvalue=='L')){
+  Serial.println('G');
+}
+else if((mtrvalue=='B')&&(strvalue=='L')){
+  Serial.println('H');
+}
+else if((mtrvalue=='B')&&(strvalue=='R')){      //  sending mode values
+  Serial.println('J');
+}
+else if((mtrvalue=='B')&&(strvalue=='S')){
+  Serial.println('B');
+}
+else if((mtrvalue=='S')&&(strvalue=='L')){
+  Serial.println('L');
+}
+else if((mtrvalue=='S')&&(strvalue=='R')){
+  Serial.println('R');
+}                                             //  to here
+else 
+Serial.println('S');
+
+
+
+if(park>60)
+parkkey=1;                            // creating key for switch so that use signal only once.
+if(parkkey==1){
+if(park<60){
+  if(parkvalue=='x'){
+    Serial.println('X');             // if off then on
+    parkvalue='X';
+  }
+  else{
+    Serial.println('x');              // if on then off
+    parkvalue='x';
+  }
+  parkkey=0;
+}
+}
+
+if(light>60)
+lightkey=1;                          // creating ey for switch so that use signal only once.
+if(lightkey==1){
+if(light<60){
+  if(lightvalue=='w'){
+    Serial.println('W');             // if off then on
+    lightvalue='W';
+  }
+  else{
+    Serial.println('w');
+    lightvalue='w';                   // if on then off
+  }
+  lightkey=0;
+}
+}
+  delay(100);
 }
