18b577b654588616dc00f0ba8b96975c4504240e
Thor8302 <68622476+Thor8302@users.noreply.github.com>
2024-09-09 17:38:52 +0530

    GHINI REMOTE PROTOCOL 2/12/2021                ghini remote : recalibrated

--- Bluetooth Remote/Ghini_remote/Ghini_remote.ino
+++ Bluetooth Remote/Ghini_remote/Ghini_remote.ino
@@ -1,71 +1,55 @@
 /*
-               GHINI REMOTE PROTOCOL 12/6/2021
-               ghini remote : - ) front value changed from 690 to 680
+               GHINI REMOTE PROTOCOL 2/12/2021
+               ghini remote : recalibrated
 */
 
-int mtrspeed, steer, light, park, strangleint, parkkey, lightkey,blu=0;
-char mtrvalue, strvalue, strangle, parkvalue = 'x', lightvalue = 'w',mtrspeedchar;
+int mtrspeed, steer, light, park, strangleint, parkkey, lightkey;
+char mtrvalue, strvalue, strangle, parkvalue = 'x', lightvalue = 'w', mtrspeedchar;
 void setup() {
   pinMode(A6, INPUT);
   pinMode(A7, INPUT);
   pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  pinMode(A3,OUTPUT);
-  pinMode(13,OUTPUT);
- digitalWrite(A3,1); 
+  pinMode(A3, OUTPUT);
+  pinMode(13, OUTPUT);
+  digitalWrite(A3, 1);
   Serial.begin(9600);
 }
 
 void loop() {
-char a;
-a='~';
-if(Serial.available())
-a=Serial.read();
-if(a!='~')
-{
-  blu=1;
-}
-else
-{
-  if(blu==1)
-  {
-    blu=0;
-    digitalWrite(A3,0);
-    delay(10);
-    digitalWrite(A3,1);
-  }
-}
 
- mtrspeed = analogRead(A6);
+  mtrspeed = analogRead(A6);
   steer = analogRead(A7);
   light = analogRead(A4);
   park = analogRead(A2);
-/*  Serial.print("mtr :");
-  Serial.println(mtrspeed);
-  Serial.print("steer");
-  Serial.println(steer);
-  */if (mtrspeed >= 680) {         //  check for forward
+  /*  Serial.print("mtr :");
+    Serial.println(mtrspeed);
+    Serial.print("steer");
+    Serial.println(steer);
+    
+  */
+  if (mtrspeed >= 710) {         //  check for forward
     mtrvalue = 'F';                       // and speed accordingly
-    mtrspeed = map(mtrspeed, 680, 1015, 32, 64);
+    mtrspeed = map(mtrspeed, 710, 1015, 32, 64);
   }
-  else if (mtrspeed <= 430) {    //check for backward
+  else if (mtrspeed <= 335) {    //check for backward
     mtrvalue = 'B';                         // and speed accordingly
-    mtrspeed = map(mtrspeed, 0, 420, 64, 32);
+    mtrspeed = map(mtrspeed, 0, 335, 64, 32);
   }
   else {
     mtrvalue = 'S';                      // else speed
     mtrspeed = 32;
   }
-    if(mtrspeed>64)
-    mtrspeed=64;
-    if(mtrspeed<32)
-    mtrspeed=32;
-    mtrspeedchar= char(mtrspeed);
-    Serial.println(mtrspeedchar);               // sending motor speed
-
-  if (steer >= 440) {                  //check for right
+  if (mtrspeed > 64)
+    mtrspeed = 64;
+  if (mtrspeed < 32)
+    mtrspeed = 32;
+  mtrspeedchar = char(mtrspeed);
+  Serial.println(mtrspeedchar);               // sending motor speed
+ // Serial.print("steer=");
+  //Serial.println(steer);
+  if (steer >= 420) {                  //check for right
     strvalue = 'R';
-    strangleint = map(steer, 440, 665, 89, 113); //and angle int value
+    strangleint = map(steer, 420, 665, 89, 113); //and angle int value
   }
   else if (steer <= 225) {              // check for left
     strvalue = 'L';
@@ -77,11 +61,11 @@ else
   }
 
 
-  if ((strangleint < 89)&&(strangleint!=75))
+  if ((strangleint < 89) && (strangleint != 75))
     strangleint = 89;
   else if (strangleint == 75)
     strangleint = 75;
- 
+
   else if (strangleint > 113)
     strangleint = 113;
   strangle = char (strangleint);
@@ -91,43 +75,43 @@ else
 
   if ((mtrvalue == 'F') && (strvalue == 'S')) { // from here
     Serial.println('F');
-    parkvalue='x';
-    lightvalue='w';
+    parkvalue = 'x';
+    lightvalue = 'w';
   }
   else if ((mtrvalue == 'F') && (strvalue == 'R')) {
     Serial.println('I');
-    parkvalue='x';
-    lightvalue='w';
+    parkvalue = 'x';
+    lightvalue = 'w';
   }
   else if ((mtrvalue == 'F') && (strvalue == 'L')) {
     Serial.println('G');
-    parkvalue='x';
-    lightvalue='w';
+    parkvalue = 'x';
+    lightvalue = 'w';
   }
   else if ((mtrvalue == 'B') && (strvalue == 'L')) {
     Serial.println('H');
-    parkvalue='x';
-    lightvalue='w';
+    parkvalue = 'x';
+    lightvalue = 'w';
   }
   else if ((mtrvalue == 'B') && (strvalue == 'R')) { //  sending mode values
     Serial.println('J');
-    parkvalue='x';
-    lightvalue='w';
+    parkvalue = 'x';
+    lightvalue = 'w';
   }
   else if ((mtrvalue == 'B') && (strvalue == 'S')) {
     Serial.println('B');
-    parkvalue='x';
-    lightvalue='w';
+    parkvalue = 'x';
+    lightvalue = 'w';
   }
   else if ((mtrvalue == 'S') && (strvalue == 'L')) {
     Serial.println('L');
-    parkvalue='x';
-    lightvalue='w';
+    parkvalue = 'x';
+    lightvalue = 'w';
   }
   else if ((mtrvalue == 'S') && (strvalue == 'R')) {
     Serial.println('R');
-    parkvalue='x';
-    lightvalue='w';
+    parkvalue = 'x';
+    lightvalue = 'w';
   }                                             //  to here
   else
     Serial.println('S');
@@ -165,6 +149,10 @@ else
       lightkey = 0;
     }
   }
- 
+
+if((park<60)&&(light<60))
+digitalWrite(A3,0);
+else
+digitalWrite(A3,1);
   delay(69);
 }
