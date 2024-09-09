72b31218b24ca3da8fb4e5ce34cc9abc463713b3
Thor8302 <68622476+Thor8302@users.noreply.github.com>
2024-09-09 17:15:06 +0530

    Lamourghini Aventador 30/5/2021          ghini update : tried to increase light power          blinking lights fn changed

--- Bluetooth Car/Lambourghini_Aventador/Lambourghini_Aventador.ino
+++ Bluetooth Car/Lambourghini_Aventador/Lambourghini_Aventador.ino
@@ -1,10 +1,11 @@
 /*
-      Lamourghini Aventador 30/5/2021
-      ghini updated , angle more precise, motor speed more precise and decreased battery drainage hopefully when speed not full
+         Lamourghini Aventador 30/5/2021
+         ghini update : tried to increase light power
+         blinking lights fn changed
 */
 #include<Servo.h>
 char t;
-int m = 0, n = 0, mtrdrv = 0, mtrcnt = 0, fcnt = 0, flght = 0, sgnlcnt = 0, sgnl = 0, angle=24,remotelock=0,once=0;
+int m = 1, n = 0, mtrdrv = 0, mtrcnt = 0, fcnt = 0, flght = 0, sgnlcnt = 0, sgnl = 0, angle = 24, remotelock = 0, once = 0;
 int s = 0, speed = 0;
 Servo steer;
 void setup() {
@@ -15,6 +16,7 @@ void setup() {
   pinMode(A1, INPUT);   //from motordrive to switch on bluetooth
   pinMode(6, OUTPUT);  //bluetooth on/off
   pinMode(8, OUTPUT);  //front light on/off
+  pinMode(7, OUTPUT);  //front light on/off BOTH FOR BETTER GRND
   Serial.begin(9600);
 
 }
@@ -28,45 +30,45 @@ void loop() {
     Serial.println(s);
     //Serial.println(langle);
     //Serial.println(rangle);
-    once=0;
+    once = 0;
   }
-   
- 
-  
 
-if(t=='K')
-remotelock=1;
 
- if(remotelock==1){                                       ///  from here
- if((s>=89)&&(s<=113))
- {
-   angle=map(s,89,113,1,25);             // angle setting column 
- }
- 
+
+
+  if (t == 'K')
+    remotelock = 1;
+
+  if (remotelock == 1) {                                   ///  from here
+    if ((s >= 89) && (s <= 113))
+    {
+      angle = map(s, 89, 113, 1, 25);       // angle setting column
+    }
+
   }                                              //// to here
 
 
-  if(remotelock==0){
-  if (t == 'q')              // from here ....
-  {
-    speed = 255;
-  }                          //this is speed setting column for phone
-  
-  if ((s >= 48) && (s <= 57))
-  {
-    s = s - 48;
-    speed = (25.5 * s);
+  if (remotelock == 0) {
+    if (t == 'q')              // from here ....
+    {
+      speed = 255;
+    }                          //this is speed setting column for phone
 
-  }                               // to here ....
-  
-}                              // from here 
-if(remotelock==1)
-{
-  if((s>=32)&&(s<=64))
-  {                                // this is speed setting column for remote
-    speed=map(s,32,64,0,255);
-  }
-}                                    // to here
+    if ((s >= 48) && (s <= 57))
+    {
+      s = s - 48;
+      speed = (25.5 * s);
+
+    }                               // to here ....
+
+  }                              // from here
+  if (remotelock == 1)
+  {
+    if ((s >= 32) && (s <= 64))
+    { // this is speed setting column for remote
+      speed = map(s, 32, 64, 0, 255);
+    }
+  }                                    // to here
 
 
 
@@ -76,10 +78,11 @@ if(remotelock==1)
     steer.write(111);
     digitalWrite(11, HIGH);
     digitalWrite(3, LOW);
-    digitalWrite(8, HIGH);
+    digitalWrite(8, 0);
+    digitalWrite(7, 0);
     fcnt = 0;
-    flght = 1;
-    m = 0;
+    flght = 0;
+    m = 1;
     n = 0;
     sgnl = 0;
 
@@ -95,13 +98,13 @@ if(remotelock==1)
     steer.write(111);
     digitalWrite(11, HIGH);
     digitalWrite(5, LOW);
-    digitalWrite(8, LOW);
-    fcnt = 100;
+    //  digitalWrite(8, LOW);
+    // fcnt = 100;
     sgnl = 0;
 
 
-    flght = 0;
-    m = 0;
+    // flght = 0;
+    m = 1;
     n = 0;
     if (speed == 255)
       digitalWrite(3, 1);
@@ -110,20 +113,26 @@ if(remotelock==1)
   }
 
   else if (t == 'L') {       //left
-    digitalWrite(11, m);
-    steer.write(111+angle);
+    if (m == 1)
+      digitalWrite(11, 0);
+    if (m == 0)
+      digitalWrite(11, 1);
+    steer.write(111 + angle);
     //digitalWrite(8,LOW);
-    m = 0;
+    m = 1;
 
     n = 0;
 
   }
 
   else if (t == 'R') {       //right
-    digitalWrite(11, m);
-    steer.write(111-angle);
+    if (m == 1)
+      digitalWrite(11, 0);
+    if (m == 0)
+      digitalWrite(11, 1);
+    steer.write(111 - angle);
     //digitalWrite(8,LOW);
-    m = 0;
+    m = 1;
 
     n = 0;
 
@@ -132,12 +141,13 @@ if(remotelock==1)
   else if (t == 'G') {  //forward left
     digitalWrite(11, HIGH);
     digitalWrite(3, LOW);
-    steer.write(111+angle);
-    digitalWrite(8, HIGH);
+    steer.write(111 + angle);
+    digitalWrite(8, 0);
+    digitalWrite(7, 0);
     fcnt = 0;
     sgnl = 0;
-    flght = 1;
-    m = 0;
+    flght = 0;
+    m = 1;
     n = 0;
 
     if (speed == 255)
@@ -148,12 +158,13 @@ if(remotelock==1)
   else if (t == 'I') { //forward right
     digitalWrite(3, LOW);
     digitalWrite(11, HIGH);
-    steer.write(111-angle);
-    digitalWrite(8, HIGH);
+    steer.write(111 - angle);
+    digitalWrite(8, 0);
+    digitalWrite(7, 0);
     fcnt = 0;
     sgnl = 0;
-    flght = 1;
-    m = 0;
+    flght = 0;
+    m = 1;
     n = 0;
     if (speed == 255)
       digitalWrite(5, 1);
@@ -163,12 +174,12 @@ if(remotelock==1)
   else if (t == 'H') { //backward left
     digitalWrite(11, HIGH);
     digitalWrite(5, LOW);
-    steer.write(111+angle);
-    digitalWrite(8, LOW);
-    fcnt = 100;
+    steer.write(111 + angle);
+    //  digitalWrite(8, LOW);
+    // fcnt = 100;
     sgnl = 0;
-    flght = 0;
-    m = 0;
+    //  flght = 0;
+    m = 1;
     n = 0;
     if (speed == 555)
       digitalWrite(3, 1);
@@ -176,14 +187,14 @@ if(remotelock==1)
       analogWrite(3, speed);
   }
   else if (t == 'J') { //backward right
-    steer.write(111-angle);
+    steer.write(111 - angle);
     digitalWrite(11, HIGH);
     digitalWrite(5, LOW);
-    digitalWrite(8, LOW);
-    fcnt = 100;
+    //  digitalWrite(8, LOW);
+    // fcnt = 100;
     sgnl = 0;
-    flght = 0;
-    m = 0;
+    // flght = 0;
+    m = 1;
     n = 0;
     if (speed == 255)
       digitalWrite(3, 1);
@@ -194,83 +205,92 @@ if(remotelock==1)
 
 
   else if (t == 'S') {    //STOP (all motors stop)
-    digitalWrite(11, m);
+    if (m == 0)
+      digitalWrite(11, 1);
+    if (m == 1)
+      digitalWrite(11, 0);
     digitalWrite(8, flght);
+    digitalWrite(7, flght);
     steer.write(111);
-    angle = 25;
-    
+    // angle = 25;
+
     n = 0;
 
   }
   else if (t == 'W') { //lights on
-    
+
     digitalWrite(5, 1);
     digitalWrite(3, 1);
-    flght = 1;
-    m = 1;
+    flght = 0;
+    m = 0;
     sgnl = 0;
     n = 0;
   }
   else if (t == 'w') { // lights off
-    
+
     digitalWrite(5, 0);
     digitalWrite(3, 0);
-    flght = 0;
+    flght = 1;
     sgnl = 0;
-    m = 0;
+    m = 1;
     n = 0;
   }
   else if (t == 'X') { // parking lights on
-   
+
     sgnl = 1;
   }
   else if (t == 'x') {
-   
+
     sgnl = 0;         // parking lights off
   }
   else if (t == 'V' || t == 'v') { // emergency all off
     digitalWrite(5, LOW);
     digitalWrite(3, LOW);
     steer.write(111);
-    digitalWrite(8, LOW);
+    digitalWrite(8, 1);
+    digitalWrite(7, 1);
     n = 0;
-    m = 0;
+    m = 1;
     sgnl = 0;
 
     digitalWrite(11, 0);
   }
   else if (t == 'A') { // no signal then off
- 
+
     n = n + 1;
-    if (n >= 20) {
-      n = 0;
-      m = 0;
+    if ((n >= 20) && (n <= 22)) {
+      //n = 0;
+      m = 1;
 
       if (sgnl == 0)
       {
         digitalWrite(5, LOW);
         digitalWrite(3, LOW);
         digitalWrite(11, 1);
-        digitalWrite(8, 0);
+        digitalWrite(8, 1);
+        digitalWrite(7, 1);
+        angle = 25;
       }
       steer.write(111);
-        if(once==0)
-   {
-     digitalWrite(6,0);
-     delay(10);
-     digitalWrite(6,1);
-     once=1;
-   }  
+      if (once == 0)
+      {
+        digitalWrite(6, 0);
+        delay(10);
+        digitalWrite(6, 1);
+        once = 1;
+      }
     }
   }
 
 
-  if (fcnt <= 100)                // from here ....
+  if (fcnt <= 250)                // from here ....
   {
     fcnt = fcnt + 1;
-    if (fcnt >= 100)             //this is timer portocol for headlight while motion.
+    if (fcnt >= 250)             //this is timer portocol for headlight while motion.
     {
       flght = m;
+      digitalWrite(8, m);
+      digitalWrite(7, m);
     }
   }
   else
@@ -294,17 +314,66 @@ if(remotelock==1)
   }
   if (sgnl == 1) {             // from here
     sgnlcnt = sgnlcnt + 1;
-    if ((sgnlcnt >= 50) && (sgnlcnt < 1000)) {
-      digitalWrite(8, 0);
+    if ((sgnlcnt >= 25 ) && (sgnlcnt < 1000)) {
+      digitalWrite(8, 1);
+      digitalWrite(7, 1);
       digitalWrite(5, 0);
       digitalWrite(3, 0);
       digitalWrite(11, 0);
-      m = 0;                    // parking light blinking function
-      flght = 0;
+      m = 1;                    // parking light blinking function
+      flght = 1;
       sgnlcnt = 1000;
     }
     if (sgnlcnt >= 1200) {
+      digitalWrite(8, 0);
+      digitalWrite(7, 0);
+      digitalWrite(5, 0);
+      digitalWrite(3, 0);
+      digitalWrite(11, 0);
+      flght = 0;
+      m = 0;
+    }
+    if (sgnlcnt >= 1225) {
       digitalWrite(8, 1);
+      digitalWrite(7, 1);
+      digitalWrite(5, 0);
+      digitalWrite(3, 0);
+      digitalWrite(11, 0);
+      flght = 1;
+      m = 1;
+    }
+    if (sgnlcnt >= 1250) {
+      digitalWrite(8, 0);
+      digitalWrite(7, 0);
+      digitalWrite(5, 0);
+      digitalWrite(3, 0);
+      digitalWrite(11, 0);
+      flght = 0;
+      m = 0;
+    }
+    if (sgnlcnt >= 1275) {
+      digitalWrite(8, 1);
+      digitalWrite(7, 1);
+      digitalWrite(5, 1);
+      digitalWrite(3, 1);
+      digitalWrite(11, 1);
+      flght = 1;
+      m = 1;
+      // sgnlcnt = 0;
+    }
+    if (sgnlcnt >= 1300) {
+      digitalWrite(8, 1);
+      digitalWrite(7, 1);
+      digitalWrite(5, 0);
+      digitalWrite(3, 0);
+      digitalWrite(11, 0);
+      flght = 1;
+      m = 1;
+      //  sgnlcnt = 0;
+    }
+    if (sgnlcnt >= 1325) {
+      digitalWrite(8, 1);
+      digitalWrite(7, 1);
       digitalWrite(5, 1);
       digitalWrite(3, 1);
       digitalWrite(11, 1);
